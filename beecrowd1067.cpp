7ac02e1647d4d65486279b5161d736fc665f58e0
José Henrique <josephenrycastro@gmail.com>
2026-08-13 17:16:11 -0300

    beecrowd1067.cpp

---
+++ beecrowd1067.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int num_impares; cin >> num_impares;
+
+    for (int i=1;i<=num_impares;i++){
+        if(i%2!=0) cout << i << endl;
+    }
+
+    return 0;
+}
