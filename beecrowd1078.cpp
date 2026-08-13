46e5e400c1a8c8b04a559fab0dd0f403b83a4ca0
José Henrique <josephenrycastro@gmail.com>
2026-08-13 17:21:09 -0300

    beecrowd1078.cpp

---
+++ beecrowd1078.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int num; cin >> num;
+
+for(int i=1;i<=10;i++){
+
+    int res = num*i;
+
+    cout << i << " x " << num << " = " << res << endl;
+
+}
+
+    return 0;
+}
